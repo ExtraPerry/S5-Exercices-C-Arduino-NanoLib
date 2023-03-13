580c75a8a67d3cc097a727a96ea6520ff83fc882
VANOT Alexy <vanot@et.intechinfo.fr>
2023-03-13 10:06:59 +0100

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+*/
+
+int main(void) {
+  
+  
+  return 0;
+}
